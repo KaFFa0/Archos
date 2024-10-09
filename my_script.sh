1a22b1fa5858a3091d0acd53f1861cefd82b89e2
Rodion <kaffa991@mail.ru>
2024-10-09 19:50:27 +0300

    final

--- my_script.sh
+++ my_script.sh
@@ -3,7 +3,15 @@
 LOG_DIR=$1 #../log
 MAX_USAGE=$2
 N=$3
+ARGS=$#
 BACKUP_DIR="./backup"
+
+if [ "$ARGS" -eq 0 ]; then
+echo "Use this instruction to run the script:"
+echo "./my_script.sh /path/to/log <max_usage> <num_of_files_to_archive>"
+exit 0
+fi
+
 
 USAGE=$(df -h | awk 'NR==3 {print $5}' | sed 's/%//')
 #echo $USAGE
@@ -13,7 +21,7 @@ if [ "$USAGE" -ge "$MAX_USAGE" ]; then
   TO_ARCH=$(find "$LOG_DIR" -type f -printf '%T@ %p\n' | sort -n | head -n "$N" | cut -d' ' -f2-)
 else 
   echo "Enough space"
-  exit 1
+  exit 0
 fi
 
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
